3d4fbbb43b4f12e91ccae252aa872a0a9b00893f
Aniekan-Samson <aniekansamsonubong@gmail.com>
2024-02-05 08:16:17 +0100

    fizbuzz array

--- practice.js
+++ practice.js
@@ -12,3 +12,21 @@
     return arr;
 }
 console.log(factorsOf(8))
+
+/** Write a function fizzBuzz(max) that accepts a number as an arg. The function should return an array
+ containing all positive numbers less than max that are divisible by either 3 or 5, but not both.**/
+
+ function fizz(num) {
+    // first define an empty array
+    let newArr = []
+    // i iniitialized the initial couter varibale i = 1 because 1 % 3 == 1 that means not divisible
+    for(let i = 1; i < num; i++) {
+        if ( i % 3 === 0 || i % 5 === 0) {
+            newArr.push(i);
+        }
+
+    }
+    return newArr
+}
+
+console.log(fizz(20))
