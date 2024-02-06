f73fa3486272ccd3fe2e7680ca34808a8d8a944b
Aniekan-Samson <aniekansamsonubong@gmail.com>
2024-02-06 04:55:04 +0100

    updated first and last function

--- practice.js
+++ practice.js
@@ -1,125 +1,125 @@
-// /**Write a function factorsOf(num) that takes in a number as an arg. The method should return an array containing all positive numbers that are able to divide into num
-//  with no remainder.**/
-//  //console.log(factorsOf(5)); // [ 1, 5 ]
-//  function factorsOf(num) {
-//     let arr = [];
+/**Write a function factorsOf(num) that takes in a number as an arg. The method should return an array containing all positive numbers that are able to divide into num
+ with no remainder.**/
+ //console.log(factorsOf(5)); // [ 1, 5 ]
+ function factorsOf(num) {
+    let arr = [];
 
-//     for (let i = 1; i <= num; i++) {
-//         if ( num % i == 0) {
-//              arr.push(i);
-//         }
-//     }
-//     return arr;
-// }
-// console.log(factorsOf(8))
+    for (let i = 1; i <= num; i++) {
+        if ( num % i == 0) {
+             arr.push(i);
+        }
+    }
+    return arr;
+}
+console.log(factorsOf(8))
 
 // /** Write a function fizzBuzz(max) that accepts a number as an arg. The function should return an array
 //  containing all positive numbers less than max that are divisible by either 3 or 5, but not both.**/
 
-//  function fizz(num) {
-//     // first define an empty array
-//     let newArr = []
-//     // i iniitialized the initial couter varibale i = 1 because 1 % 3 == 1 that means not divisible
-//     for(let i = 1; i < num; i++) {
-//         if ( i % 3 === 0 || i % 5 === 0) {
-//             newArr.push(i);
-//         }
+ function fizz(num) {
+    // first define an empty array
+    let newArr = []
+    // i iniitialized the initial couter varibale i = 1 because 1 % 3 == 1 that means not divisible
+    for(let i = 1; i < num; i++) {
+        if ( i % 3 === 0 || i % 5 === 0) {
+            newArr.push(i);
+        }
 
-//     }
-//     return newArr
-// }
+    }
+    return newArr
+}
 
-// console.log(fizz(20))
+console.log(fizz(20))
 
 // /** Write a function pitPat(max) that accepts a number as an arg. The function should return an
 // array containing all positive numbers less than or equal to max that are divisible by either 4 or 6, but not both.**/
-// function pitPat(max) {
-//     let arr = [];
+function pitPat(max) {
+    let arr = [];
 
-//     for (let i = 1; i <= max; i++) {
-//         if (i % 4 === 0 && i % 6 === 0) {
-//             arr.push()
-//         } else if(i % 4 === 0) {
-//             arr.push(i);
-//         } else if (i % 6 === 0)
-//             arr.push(i);
-//     }
-//     return arr;
-// }
+    for (let i = 1; i <= max; i++) {
+        if (i % 4 === 0 && i % 6 === 0) {
+            arr.push()
+        } else if(i % 4 === 0) {
+            arr.push(i);
+        } else if (i % 6 === 0)
+            arr.push(i);
+    }
+    return arr;
+}
 
-// console.log(pitPat(18))
+console.log(pitPat(18))
 
 
 // /** Write a function tripleSequence that takes in two numbers, start and length.
 // The function should return an array representing a sequence that begins with start and is length elements long.
 // In the sequence, every element should be 3 times the previous element. Assume that the length is at least 1.**/
 
-// function tripleSequence(start, length) {
-//     if (length === 0) {
-//         return [];
-//     }
-//     // the sequence is the array that will be returned it begis with start as the base element of the array
-//     let sequence = [start];
+function tripleSequence(start, length) {
+    if (length === 0) {
+        return [];
+    }
+    // the sequence is the array that will be returned it begis with start as the base element of the array
+    let sequence = [start];
 
-//     // the list of elements in the sequence has to alwaye be less than the "length" in the argument passed into the function
-//     while (sequence.length < length) {
-//         let lastElem = sequence[sequence.length - 1];
-//         // gettig the first element of the sequence by index number and assigning it to lastElem variable
+    // the list of elements in the sequence has to alwaye be less than the "length" in the argument passed into the function
+    while (sequence.length < length) {
+        let lastElem = sequence[sequence.length - 1];
+        // gettig the first element of the sequence by index number and assigning it to lastElem variable
 
-//         let nextElem = lastElem * 3;
-//         sequence.push(nextElem);
-//     }
-//     return sequence;
-// }
+        let nextElem = lastElem * 3;
+        sequence.push(nextElem);
+    }
+    return sequence;
+}
 
-// console.log(tripleSequence(3, 5));
+console.log(tripleSequence(3, 5));
 
 // /**Write a function yeller(words) that takes in an array of words.
 //  The function should return a new array where each element of the original array is yelled**/
 
-// function yeller(words) {
-//     let newArr = [];
-//     for (let i = 0; i < words.length; i++) {
-//         let capArr = words[i].toUpperCase();
-//         newArr.push(`${capArr}!`);
-//     }
-//     return newArr;
-// }
-// console.log(yeller(["hello", "world", "getting", "better"]))
+function yeller(words) {
+    let newArr = [];
+    for (let i = 0; i < words.length; i++) {
+        let capArr = words[i].toUpperCase();
+        newArr.push(`${capArr}!`);
+    }
+    return newArr;
+}
+console.log(yeller(["hello", "world", "getting", "better"]))
 
 
-// /** Write a function tripler(nums) that takes in an array as an arg.
-// The function should return a new array containing three times every number of the original array.**/
-// function tripler(nums) {
-//     let newArr = [];
-//     for (let i = 0; i < nums.length; i++) {
-//         let multiple = 3;
-//         let testArr = nums[i] * multiple;
-//         newArr.push(testArr);
-//     }
-//     return newArr;
-// }
-// console.log(tripler([-5, 10, 0, 11]))
+/** Write a function tripler(nums) that takes in an array as an arg.
+The function should return a new array containing three times every number of the original array.**/
+function tripler(nums) {
+    let newArr = [];
+    for (let i = 0; i < nums.length; i++) {
+        let multiple = 3;
+        let testArr = nums[i] * multiple;
+        newArr.push(testArr);
+    }
+    return newArr;
+}
+console.log(tripler([-5, 10, 0, 11]))
 
-// /**Write a function longWords(words) that takes in an array of words.
-//  The function should return an array containing only the words that are longer than 5 characters.**/
+ /**Write a function longWords(words) that takes in an array of words.
+  The function should return an array containing only the words that are longer than 5 characters**/
 
-//  function longWords(words) {
-//     let newArr = [];
-//     let len = words.length;
+ function longWords(words) {
+    let newArr = [];
+    let len = words.length;
 
-//     for (let i = 0; i < len; i++) {
-//         if (words[i].length <= 5){
-//             newArr.push();
-//         } else {
-//             newArr.push(words[i]);
-//         }
-//     }
-//     return newArr;
-// }
-// console.log(longWords(["breaking", "story", "helping", "bikes", "motorist", "bike"]));
-// console.log(longWords(['bike', 'skateboard','scooter', 'moped'])); // [ 'skateboard', 'scooter' ]
-// console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka'
+    for (let i = 0; i < len; i++) {
+        if (words[i].length <= 5){
+            newArr.push();
+        } else {
+            newArr.push(words[i]);
+        }
+    }
+    return newArr;
+}
+console.log(longWords(["breaking", "story", "helping", "bikes", "motorist", "bike"]));
+console.log(longWords(['bike', 'skateboard','scooter', 'moped'])); // [ 'skateboard', 'scooter' ]
+console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka'
 
 
 
