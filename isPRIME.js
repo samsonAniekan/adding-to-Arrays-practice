4084215a198db873d7645c556f68a6faef98ff63
Aniekan-Samson <aniekansamsonubong@gmail.com>
2024-02-21 16:15:22 +0100

    choosePrimes

--- isPRIME.js
+++ isPRIME.js
@@ -35,7 +35,57 @@ function isPrime(num) {
     return true;
 }
 
-console.log(isPrime(7))
 
-console.log(primeFactors(12))
-console.log(primeFactors(6))
+console.log(primeFactors(12)) // [2, 6]
+console.log(primeFactors(6)) // [2, 6]
+console.log(primeFactors(8))
+
+
+// Write a function choosePrimes(nums) that takes in an array of numbers as args.
+//  The function should return a new array containing the primes from the original array.
+//   A prime number is a number that is only divisible by 1 and
+//  itself. Hint: consider creating a helper function to check if a number is prime!
+
+
+//check if a number is prime number using checkIfPrime helper function
+
+// create a new function called choosePrime that takes an array prime numbers
+
+// loop through every value of the choosePrime array
+// then check to see if the number at a partcicular index is a prime number
+
+// if the number is prime
+// push the number into the new empty array created above
+// return the array of new prime numbers
+
+function checkIfNumPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i < num; i++) {
+        if (num % 2 === 0) {
+            return false;
+
+        }
+
+    }
+
+    return true;
+}
+
+function choosePrimes(arr) {
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        if (checkIfNumPrime(num)) {   // by calling the checkIfNumPrime function 1, 2, 3, 6, 7, 9 are checked for prime numbers
+            newArr.push(num)
+            console.log(newArr);
+        }
+    }
+
+    return newArr;
+}
+
+console.log(choosePrimes([1, 2, 3, 6, 7, 9])) // [2, 3, 7]
